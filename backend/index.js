66e3454d2c897f676e7e78e3f2727a8e60984e05
Vinay <work.vinaythorat@gmail.com>
2026-01-24 20:57:12 +0530

    Add MongoDB connection and movie schema

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+
 const express = require("express");
 const cors = require("cors");
 
@@ -12,6 +16,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend server is running");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
